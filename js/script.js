f2e2fc9cd1ca91674a3bbf8914751573d0dd7bcc
Cody <54541144+CodyDickson@users.noreply.github.com>
2019-09-10 20:33:46 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,101 +1,42 @@
-// Image Forward / Back
+// Image Galleries
 var galleries = [
   ["images/hike1/hike1_1.jpg", "images/hike1/hike1_2.jpg", "images/hike1/hike1_3.jpg", "images/hike1/hike1_4.jpg", "images/hike1/hike1_5.jpg"],
   ["images/hike2/hike2_1.jpg", "images/hike2/hike2_2.jpg","images/hike2/hike2_3.jpg"],
   ["images/hike3/hike3_1.jpg", "images/hike3/hike3_2.jpg","images/hike3/hike3_3.jpg"]
 ];
-var index = [
-  
-];
 var hike1Index = 0;
 var hike2Index = 0;
 var hike3Index = 0;
 
+// Arrow moving forward (right)
 function changeImage(hikeNumber) {
   var img = document.getElementById(hikeNumber);
   if (hikeNumber == "hike1") {
-    ++hike1Index;
-    var max = 4;
-    var index = hike1Index;
-    var galleryIndex = 0;
-    if (hike1Index == 4) {
-      hike1Index = -1;
-    }
+    hike1Index = (hike1Index == 4) ? 0 : hike1Index + 1;
+    img.src = galleries[0][hike1Index]
   } else if (hikeNumber == "hike2") {
-    ++hike2Index;
-    var max = 2;
-    var index = hike2Index;
-    var galleryIndex = 1;
-    if (hike2Index == 2) {
-      hike2Index = -1;
-    }
+    hike2Index = (hike2Index == 2) ? 0 : hike2Index + 1;
+    img.src = galleries[1][hike2Index]
   } else if (hikeNumber == "hike3") {
-    ++hike3Index;
-    var max = 2;
-    var index = hike3Index;
-    var galleryIndex = 2;
-    if (hike3Index == 2) {
-      hike3Index = -1;
-    }
-  }
-  if (index < max) {
-    img.src = galleries[galleryIndex][index];
-  } else {
-    img.src = galleries[galleryIndex][index];
+    hike3Index = (hike3Index == 2) ? 0 : hike3Index + 1;
+    img.src = galleries[2][hike3Index]
   }
 }
 
-/* function changeImage(hikeNumber) {
-  var img = document.getElementById("hike1");
-  if (hikeNumber == "hike1") {
-    var max = hike1Max;
-    var index = hike1Index;
-  } else if (hikeNumber == "hike2") {
-    var max = hike2Max;
-    var index = hike2Index;
-  } else if (hikeNumber == "hike3") {
-    var max = hike3Max;
-    var index = hike3Max;
-  }
-  if (index == max) {
-    img.src = hike1[hike1Index];
-    index = -1;
-  } else {
-    img.src = hike1[hike1Index];
-    ++index;
-  }
-} */
-
-/*
-const forEach ()
-
-function addEventListener() {
-  document.getElementsByClassName("arrowRight").addEventListener('click', (e) => {
-  const index = e.target.dataset.gallery;
-  const gallery = galleries[index];
-  change_image(gallery);
-});
-}
-*/
-
+// Arrow moving backwards (left)
 function changeImageBack(hikeNumber) {
-  var img = document.getElementById("firstImg");
-  hike1Index = --hike1Index;
-  if (hike1Index < 0) {
-    hike1Index = 4;
-    img.src = hike1[hike1Index];
-  } else {
-    img.src = hike1[hike1Index];
+  var img = document.getElementById(hikeNumber);
+  if (hikeNumber == "hike1") {
+    hike1Index = (hike1Index == 0) ? 4 : hike1Index - 1;
+    img.src = galleries[0][hike1Index]
+  } else if (hikeNumber == "hike2") {
+    hike2Index = (hike2Index == 0) ? 2 : hike2Index - 1;
+    img.src = galleries[1][hike2Index]
+  } else if (hikeNumber == "hike3") {
+    hike3Index = (hike3Index == 0) ? 2 : hike3Index - 1;
+    img.src = galleries[2][hike3Index]
   }
 }
-
-/*   hike1Index = ++hike1Index;
-  if (hike1Index == hike1Max) {
-    img.src = hike1[hike1Index];
-    hike1Index = -1;
-  } else {
-    img.src = hike1[hike1Index];
-  }*/
 
 // Get the modal
 var modal = document.getElementById("firstModal");
